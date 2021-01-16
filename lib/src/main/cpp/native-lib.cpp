c3b97e817fb061b311cd4c2355528db96e399838
Greentaily <greentail@protonmail.ch>
2021-01-16 07:12:28 +0300

    Move lib logic from C to Java

--- lib/src/main/cpp/native-lib.cpp
+++ lib/src/main/cpp/native-lib.cpp
@@ -2,72 +2,16 @@
 #include <string>
 
 extern "C" JNIEXPORT jstring JNICALL
-Java_com_example_lib_DeviceInfo_getAndroidIdFromJNI(JNIEnv* env, jclass, jobject context) {
-    // android_id is stored inside Settings.Secure
-    // accessed via Secure.getString(ContentResolver, Secure.ANDROID_ID)
-    jclass secure_class = env->FindClass("android/provider/Settings$Secure");
-    jclass context_class = env->FindClass("android/content/Context");
-    jmethodID context_get_content_resolver = env->GetMethodID(context_class, "getContentResolver", "()Landroid/content/ContentResolver;");
-    jmethodID secure_get_string = env->GetStaticMethodID(secure_class, "getString", "(Landroid/content/ContentResolver;Ljava/lang/String;)Ljava/lang/String;");
-
-    jobject content_resolver = env->CallObjectMethod(context, context_get_content_resolver);
-    jobject android_id = env->CallStaticObjectMethod(secure_class, secure_get_string, content_resolver, env->NewStringUTF("android_id"));
+Java_com_example_lib_DeviceInfo_getAndroidIdFromJNI(JNIEnv* env, jclass jcl, jobject context) {
+    jmethodID resolve_android_id = env->GetStaticMethodID(jcl, "resolveAndroidId","(Landroid/content/Context;)Ljava/lang/String;");
+    jobject android_id = env->CallStaticObjectMethod(jcl, resolve_android_id, context);
     // not dynamic_cast as the cast is valid regardless
     return static_cast<jstring>(android_id);
 }
 
 extern "C" JNIEXPORT jobject JNICALL
-Java_com_example_lib_DeviceInfo_getInstalledPackagesFromJNI(JNIEnv* env, jclass, jobject context) {
-    /* Use PackageManager to retrieve list of installed packages */
-    /* Context.getPackageManager() */
-    jclass context_class = env->FindClass("android/content/Context");
-    jmethodID get_pm = env->GetMethodID(context_class, "getPackageManager", "()Landroid/content/pm/PackageManager;");
-    /* PackageManager.getInstalledPackages() */
-    jclass pm_class = env->FindClass("android/content/pm/PackageManager");
-    jmethodID pm_get_installed_packages = env->GetMethodID(pm_class, "getInstalledPackages","(I)Ljava/util/List;");
-    jmethodID pm_get_application_info = env->GetMethodID(pm_class, "getApplicationInfo", "(Ljava/lang/String;I)Landroid/content/pm/ApplicationInfo;");
-    /* Java List methods */
-    jclass java_list_class = env->FindClass("java/util/List");
-    jclass java_array_list_class = env->FindClass("java/util/ArrayList");
-    jmethodID list_constructor = env->GetMethodID(java_array_list_class, "<init>", "()V");
-    jmethodID list_size = env->GetMethodID(java_list_class, "size", "()I");
-    jmethodID list_get = env->GetMethodID(java_list_class, "get", "(I)Ljava/lang/Object;");
-    jmethodID list_add = env->GetMethodID(java_list_class, "add", "(Ljava/lang/Object;)Z");
-    /* PackageInfo.packageName() */
-    jclass pi_class = env->FindClass("android/content/pm/PackageInfo");
-    jfieldID pi_package_name = env->GetFieldID(pi_class, "packageName", "Ljava/lang/String;");
-    /* ApplicationInfo.flags */
-    jclass ai_class = env->FindClass("android/content/pm/ApplicationInfo");
-    jfieldID ai_flags = env->GetFieldID(ai_class, "flags", "I");
-    jfieldID ai_flag_system = env->GetStaticFieldID(ai_class, "FLAG_SYSTEM", "I");
-    /* Class com.example.lib.Package */
-    jclass lib_package_class = env->FindClass("com/example/lib/AppPackage");
-    jmethodID lib_package_constructor = env->GetMethodID(lib_package_class, "<init>","(Ljava/lang/String;IZ)V");
-
-    /* Retrieve data and populate new List with instances of Package */
-
-    jobject pm = env->CallObjectMethod(context, get_pm);
-    jobject package_info_list = env->CallObjectMethod(pm, pm_get_installed_packages, 0);
-    jint size = env->CallIntMethod(package_info_list, list_size);
-    jint application_system_flag = env->GetStaticIntField(ai_class, ai_flag_system);
-    // new List to populate with instances of Package
-    jobject output_package_list = env->NewObject(java_array_list_class, list_constructor);
-
-    for (jint i = 0; i < size; ++i) {
-        jobject package_info = env->CallObjectMethod(package_info_list, list_get, i);
-        /* Package data */
-        // package name
-        jobject package_name = env->GetObjectField(package_info, pi_package_name);
-        // package size
-        jint package_size = 10;
-        // is it a system package?
-        jobject application_info = env->CallObjectMethod(pm, pm_get_application_info, package_name, 0);
-        jint application_flags = env->GetIntField(application_info, ai_flags);
-        jboolean package_is_system = application_flags & application_system_flag;
-        // new Package
-        jobject lib_package = env->NewObject(lib_package_class, lib_package_constructor,
-                                             package_name, package_size, package_is_system);
-        env->CallBooleanMethod(output_package_list, list_add, lib_package);
-    }
-    return output_package_list;
+Java_com_example_lib_DeviceInfo_getInstalledPackagesFromJNI(JNIEnv* env, jclass jcl, jobject context) {
+    jmethodID new_package_list = env->GetStaticMethodID(jcl, "newPackageList", "(Landroid/content/Context;)Ljava/util/List;");
+    jobject package_list = env->CallStaticObjectMethod(jcl, new_package_list, context);
+    return package_list;
 }
--- lib/src/main/java/com/example/lib/DeviceInfo.java
+++ lib/src/main/java/com/example/lib/DeviceInfo.java
@@ -1,7 +1,13 @@
 package com.example.lib;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.provider.Settings;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class DeviceInfo {
@@ -14,6 +20,29 @@ public class DeviceInfo {
 
     public static List<AppPackage> getPackages(Context context) {
         return getInstalledPackagesFromJNI(context);
+    }
+
+    @SuppressLint("HardwareIds")
+    private static String resolveAndroidId(Context context) {
+        // android_id is stored inside Settings.Secure
+        // accessed via Secure.getString(ContentResolver, Secure.ANDROID_ID)
+        return Settings.Secure.getString(context.getContentResolver(), "android_id");
+    }
+
+    private static List<AppPackage> newPackageList(Context context) {
+        PackageManager package_manager = context.getPackageManager();
+        List<PackageInfo> package_info_list = package_manager.getInstalledPackages(0);
+
+        int package_count = package_info_list.size();
+        List<AppPackage> app_package_list = new ArrayList<>();
+
+        for (int i = 0; i < package_count; i++) {
+            PackageInfo package_info = package_info_list.get(i);
+            boolean is_system = ((package_info.applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) != 0);
+            AppPackage app_package = new AppPackage(package_info.packageName, 10, is_system);
+            app_package_list.add(app_package);
+        }
+        return app_package_list;
     }
 
     private static native String getAndroidIdFromJNI(Context context);
